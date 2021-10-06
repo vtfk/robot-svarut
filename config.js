792475d88e172ccd7bc5eabbe39510cb705ac24f
Rune Moskvil Lyngås <rune.moskvil.lyngaas@vtfk.no>
2021-10-06 21:25:30 +0200

    standard --fix

--- config.js
+++ config.js
@@ -14,15 +14,15 @@ module.exports = {
   PAPERTRAIL_HOSTNAME: process.env.PAPERTRAIL_HOSTNAME || 'status-robot',
   PAPERTRAIL_HOST: process.env.PAPERTRAIL_HOST || 'logs.papertrailapp.com',
   PAPERTRAIL_PORT: process.env.PAPERTRAIL_PORT || 12345,
-  OVERRIDE_RECIPIENTS: process.env.OVERRIDE_RECIPIENTS || undefined,
-  OVERRIDE_RECIPIENTS_ADDRESSES: process.env.OVERRIDE_RECIPIENTS_ADDRESSES || undefined,
+  OVERRIDE_RECIPIENTS: (process.env.OVERRIDE_RECIPIENTS && process.env.OVERRIDE_RECIPIENTS.toLowerCase() === 'true') || false,
+  OVERRIDE_RECIPIENTS_ADDRESSES: (process.env.OVERRIDE_RECIPIENTS_ADDRESSES && process.env.OVERRIDE_RECIPIENTS_ADDRESSES.split(',')) || [],
   OVERRIDE_RECIPIENTS_OBJECT: {
-    "type": "privatPerson",
-    "name": "Chuck Norris",
-    "address1": "Svend Foynsgt. 9",
-    "address2": "",
-    "address3": "",
-    "postalCode": "3126",
-    "postalCity": "Tønsberg"
+    type: 'privatPerson',
+    name: 'Chuck Norris',
+    address1: 'Svend Foynsgt. 9',
+    address2: '',
+    address3: '',
+    postalCode: '3126',
+    postalCity: 'Tønsberg'
   }
 }
